455b3b700dcd0ce8061152bfeb3b160a16a8c127
Enry Seitz <enrique6tz@gmail.com>
2023-07-12 16:43:52 -0300

    primerDesafio

--- main.js
+++ main.js
@@ -1,42 +1,50 @@
 
-class Producto{
-    constructor(title,description,price,thumbnail, code, stock){
-        this.title = title
-        this.description = description
-        this.price = price
-        this.thumbnail = thumbnail
-        this.code = code
-        this.stock = stock
-        this.id = null
-    }
-}
-
 class ProductManager {
 
     constructor(){
         this.arrayProductos = []
         this.id = 0
     }
-    addProduct(produ){
-        if(!(this.arrayProductos.some(producto => producto.code == code))){
-            produ.id = this.id
-            this.arrayProductos.push(produ)
-            this.id += 1
-        return true
-    }else
-        return false
-    }
-    codeEqual(code){
-        //retorna verdadero si el id coincide con el producto
-        return this.arrayProductos.some(producto => producto.code == code)
-    }
     getProducts (){
         return this.arrayProductos
+    }
+    addProduct (title, description, price, thumbnail, code, stock){
+        if(!this.validarCode(code)){
+        const producto ={
+            id: this.id,
+            title,
+            description, 
+            price, 
+            thumbnail, 
+            code, 
+            stock
+            }
+        this.id += 1
+        this.arrayProductos.push(producto)
+        }
+        else{
+            console.log("Error: codigo repetido, no se puede añadir")
+            return 
+        }
+    }
+    validarCode(code){
+        return this.arrayProductos.some(elem => elem.code === code);
+    }
+    getProductById(id) {
+        const product = this.arrayProductos.find((elem) => elem.id === id);
+        if (product) {
+          return product;
+        } else {
+          console.log("Error: Product not found");
+          return null;
+        }
     }
 }
 
 controlador = new ProductManager()
 controlador.getProducts()
-
+controlador.addProduct("producto prueba", "es un producto de prueba", 200, "sin imagen", "abc123", 25) 
 controlador.addProduct("producto prueba", "es un producto de prueba", 200, "sin imagen", "abc123", 25) 
 console.log(controlador.getProducts())
+controlador.getProductById(0)
+controlador.getProductById(1)
